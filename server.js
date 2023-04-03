ced4b7e8b2cc9da1d960bf671bbbe4cc74b17ee3
GrandMaster098 Lightyear <sujit.gda997@gmail.com>
2023-04-03 16:32:53 +0000

    finish

--- server.js
+++ server.js
@@ -2,36 +2,35 @@ const express = require("express")
 const connectDB = require("./config/db")
 const cors = require("cors")
 const books = require("./routes/api/books")
-const path = require("path")
+const path = require('path');
 require("dotenv").config( { path: "./config.env" } )
 
-// connect to DB
+// CONNECTING TO DB
 connectDB()
 
-// CONNECTING DB
+// INITIATING APP
 const app = express()
 
 
+
 // HANDLING MIDDLEWARE
-app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 app.use("/api/books", books)
 
+
 // STATIC FILES
-app.use(express.static(path.join(__dirname, "./client/build")))
-app.get("*", function(_,res){
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
     res.sendFile(
-        path.join(__dirname,"./client/build/index.html"),
-        function(err){
-            res.status(500).send(err)
+        path.join(__dirname, "./client/build/index.html"),
+        function (err) {
+            res.status(500).send(err);
         }
-    )
-})
+    );
+});
 
 
+// STARTING SERVER
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-
-
+app.listen(port, () => console.log(`Express server running on port ${port}`));
